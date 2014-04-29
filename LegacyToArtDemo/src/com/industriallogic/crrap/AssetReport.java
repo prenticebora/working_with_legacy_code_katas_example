28f21e55a707949620eafb22bd6007e6494fc83b
brliu <boraliu@gmail.com>
2014-04-29 14:16:03 +0800

    Kata 5.2 Move cohensive methods (and data) to the right class

--- LegacyToArtDemo/src/com/industriallogic/crrap/AssetReport.java
+++ LegacyToArtDemo/src/com/industriallogic/crrap/AssetReport.java
@@ -33,7 +33,7 @@ public class AssetReport {
 	public void execute(RecordSet records, PrintWriter writer) {
 		assetReportData.initAssetReportData();
 
-		assetReportData.totalPositions = calcRisk(records);
+		assetReportData.totalPositions = assetReportData.calcRisk(records);
 
 		printAssetRisk(writer);
 	}
@@ -89,60 +89,6 @@ public class AssetReport {
 		}
 		writer.write("</groups>\n");
 		writer.flush();
-	}
-
-	private BigDecimal calcRisk(RecordSet records) {
-		for (int row = 0; row < records.getRowCount(); row++) {
-			BigDecimal positioning = new BigDecimal(1);
-			BigDecimal risk = new BigDecimal("0.00");
-
-			String issue = new String("");
-			issue = records.getItem(row, "ISSUE_NAME");
-			if (records.getItem(row, "ISSUE_FAMILY").toUpperCase()
-					.startsWith("FUND")) {
-				// pos = quantity * (market-unit price[TERM_TWO])
-				BigDecimal perItem = records.getDecimal(row, "MARKET_PRICE")
-						.subtract(records.getDecimal(row, "TERM_ONE"));
-				positioning = perItem.multiply(
-						records.getDecimal(row, "QUANTITY")).setScale(2,
-						BigDecimal.ROUND_HALF_UP);
-				BigDecimal riskCoefficient = assetReportData.assessor
-						.getRiskCoefficient(
-								records.getItem(row, "ISSUE_FAMILY"),
-								records.getDecimal(row, "TERM_TWO"));
-				BigDecimal product = riskCoefficient.multiply(positioning);
-				risk = product.divide(new BigDecimal("100.00"), 2,
-						BigDecimal.ROUND_HALF_UP);
-				assetReportData.positions.put(issue, positioning);
-			} else {
-				// pos = (quantity * market) - total price[TERM_ONE]
-				positioning = records.getDecimal(row, "QUANTITY").multiply(
-						records.getDecimal(row, "MARKET_PRICE"));
-				positioning = positioning.subtract(
-						records.getDecimal(row, "TERM_ONE")).setScale(2,
-						BigDecimal.ROUND_HALF_UP);
-				BigDecimal product = records.getDecimal(row, "TERM_TWO")
-						.multiply(positioning);
-				risk = product.divide(new BigDecimal("100.00"), 2,
-						BigDecimal.ROUND_HALF_UP);
-				assetReportData.positions.put(issue, positioning);
-			}
-			assetReportData.totalPositions = assetReportData.totalPositions
-					.add(assetReportData.positions.get(issue));
-
-			String group = records.getItem(row, "ISSUE_GROUP");
-			String name = records.getItem(row, "ISSUE_NAME");
-			assetReportData.assetToGroup.put(name, group);
-			BigDecimal value = new BigDecimal("0");
-
-			if (assetReportData.groupTotal.containsKey(group))
-				value = value.add(assetReportData.groupTotal.get(group))
-						.setScale(2);
-			value = value.add(assetReportData.positions.get(issue));
-			assetReportData.groupTotal.put(group, value.setScale(2));
-			assetReportData.riskTables.put(issue, risk);
-		}
-		return assetReportData.totalPositions;
 	}
 
 }
