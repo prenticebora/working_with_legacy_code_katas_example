e606657f9e83bfb7d4591355681442d90d61aafc
brliu <boraliu@gmail.com>
2014-04-29 13:57:39 +0800

    Kata 1: Format for future diff and rename to make the code more understandable

--- LegacyToArtDemo/src/com/industriallogic/crrap/AssetReport.java
+++ LegacyToArtDemo/src/com/industriallogic/crrap/AssetReport.java
@@ -34,11 +34,11 @@ public class AssetReport {
 
 	public void execute(RecordSet records, PrintWriter writer) {
 		TreeMap<String, BigDecimal> groupTotal;
-		
+
 		// Store positions for all assets
 		TreeMap<String, BigDecimal> positions;
 		BigDecimal allPositions;
-		
+
 		// Risks for all assets
 		HashMap<String, BigDecimal> m_hmRiskTable;
 		HashMap<String, String> assetToGroup;
@@ -48,30 +48,41 @@ public class AssetReport {
 		allPositions = new BigDecimal("0.00");
 		assetToGroup = new HashMap<String, String>();
 		for (int row = 0; row < records.getRowCount(); row++) {
-			BigDecimal pos = new BigDecimal(1);
-			BigDecimal r = new BigDecimal("0.00");
+			BigDecimal positioning = new BigDecimal(1);
+			BigDecimal risk = new BigDecimal("0.00");
+
 			String issue = new String("");
 			issue = records.getItem(row, "ISSUE_NAME");
-			if (records.getItem(row, "ISSUE_FAMILY").toUpperCase().startsWith("FUND")) {
+			if (records.getItem(row, "ISSUE_FAMILY").toUpperCase()
+					.startsWith("FUND")) {
 				// pos = quantity * (market-unit price[TERM_TWO])
-				BigDecimal perItem = records.getDecimal(row, "MARKET_PRICE").subtract(records.getDecimal(row, "TERM_ONE"));
-				pos = perItem.multiply(records.getDecimal(row, "QUANTITY")).setScale(2, BigDecimal.ROUND_HALF_UP);
-				BigDecimal riskCoefficient = assessor.getRiskCoefficient(records.getItem(row, "ISSUE_FAMILY"), records
-						.getDecimal(row, "TERM_TWO"));
-				BigDecimal product = riskCoefficient.multiply(pos);
-				r = product.divide(new BigDecimal("100.00"), 2, BigDecimal.ROUND_HALF_UP);
-				positions.put(issue, pos);
-			}
-			else {
+				BigDecimal perItem = records.getDecimal(row, "MARKET_PRICE")
+						.subtract(records.getDecimal(row, "TERM_ONE"));
+				positioning = perItem.multiply(
+						records.getDecimal(row, "QUANTITY")).setScale(2,
+						BigDecimal.ROUND_HALF_UP);
+				BigDecimal riskCoefficient = assessor.getRiskCoefficient(
+						records.getItem(row, "ISSUE_FAMILY"),
+						records.getDecimal(row, "TERM_TWO"));
+				BigDecimal product = riskCoefficient.multiply(positioning);
+				risk = product.divide(new BigDecimal("100.00"), 2,
+						BigDecimal.ROUND_HALF_UP);
+				positions.put(issue, positioning);
+			} else {
 				// pos = (quantity * market) - total price[TERM_ONE]
-				pos = records.getDecimal(row, "QUANTITY").multiply(records.getDecimal(row, "MARKET_PRICE"));
-				pos = pos.subtract(records.getDecimal(row, "TERM_ONE")).setScale(2, BigDecimal.ROUND_HALF_UP);
-				BigDecimal product = records.getDecimal(row, "TERM_TWO").multiply(pos);
-				r = product.divide(new BigDecimal("100.00"), 2, BigDecimal.ROUND_HALF_UP);
-				positions.put(issue, pos);
+				positioning = records.getDecimal(row, "QUANTITY").multiply(
+						records.getDecimal(row, "MARKET_PRICE"));
+				positioning = positioning.subtract(
+						records.getDecimal(row, "TERM_ONE")).setScale(2,
+						BigDecimal.ROUND_HALF_UP);
+				BigDecimal product = records.getDecimal(row, "TERM_TWO")
+						.multiply(positioning);
+				risk = product.divide(new BigDecimal("100.00"), 2,
+						BigDecimal.ROUND_HALF_UP);
+				positions.put(issue, positioning);
 			}
 			allPositions = allPositions.add(positions.get(issue));
-		
+
 			String group = records.getItem(row, "ISSUE_GROUP");
 			String name = records.getItem(row, "ISSUE_NAME");
 			assetToGroup.put(name, group);
@@ -80,35 +91,45 @@ public class AssetReport {
 				value = value.add(groupTotal.get(group)).setScale(2);
 			value = value.add(positions.get(issue));
 			groupTotal.put(group, value.setScale(2));
-			m_hmRiskTable.put(issue, r);
+			m_hmRiskTable.put(issue, risk);
 		}
-		
+
 		writer.write("<groups>\n");
 		// groups in sorted order
-		Iterator<String> g = groupTotal.keySet().iterator();
-		while (g.hasNext()) {
-			String grp = g.next();
+		Iterator<String> groups = groupTotal.keySet().iterator();
+		while (groups.hasNext()) {
+			String grp = groups.next();
+
 			BigDecimal position = groupTotal.get(grp);
 			BigDecimal product = position.multiply(new BigDecimal(100));
-			BigDecimal weight = product.divide(allPositions, 2, BigDecimal.ROUND_HALF_UP);
-			writer.write("\t<group position='" + position.toPlainString());
-			writer.write("' weight='" + weight);
+			BigDecimal weight = product.divide(allPositions, 2,
+					BigDecimal.ROUND_HALF_UP);
+			writer.write("\t<group position='"
+					+ position.toPlainString());
+			writer.write("' weight='"
+					+ weight);
 			writer.write("'>\n");
-			writer.write("\t\t" + grp + "\n");
-			Iterator<String> i = positions.keySet().iterator();
+			writer.write("\t\t"
+					+ grp + "\n");
+			Iterator<String> iter = positions.keySet().iterator();
 			boolean notFirstOne = false;
-			while (i.hasNext()) {
-				String asset = i.next();
+			while (iter.hasNext()) {
+				String asset = iter.next();
 				// Output asset only if it belongs in group
 				if (assetToGroup.get(asset).equalsIgnoreCase(grp)) {
 					if (notFirstOne)
 						writer.write("\n");
-					writer.write("\t\t<asset position='" + positions.get(asset).toPlainString() + "' ");
+					writer.write("\t\t<asset position='"
+							+ positions.get(asset).toPlainString() + "' ");
 					BigDecimal p = positions.get(asset);
-					BigDecimal weight1 = p.multiply(new BigDecimal("100.00")).divide(position, 2, BigDecimal.ROUND_HALF_UP)
+					BigDecimal weight1 = p.multiply(new BigDecimal("100.00"))
+							.divide(position, 2, BigDecimal.ROUND_HALF_UP)
 							.setScale(2);
-					writer.write("weight='" + weight1 + "' risk='" + m_hmRiskTable.get(asset).toPlainString() + "'>\n");
-					writer.write("\t\t\t" + asset + "\n");
+					writer.write("weight='"
+							+ weight1 + "' risk='"
+							+ m_hmRiskTable.get(asset).toPlainString() + "'>\n");
+					writer.write("\t\t\t"
+							+ asset + "\n");
 					writer.write("\t\t</asset>");
 					notFirstOne = true;
 				}
